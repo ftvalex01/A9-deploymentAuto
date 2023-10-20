ad1363b301c2873927c3ccc6878216a614f79302
ftvalex01 <ftvalex@hotmail.com>
2023-10-20 21:26:34 +0100

    ejercicio 1 A10 opcache

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    dd(opcache_get_status());
     return view('welcome');
 });
 
